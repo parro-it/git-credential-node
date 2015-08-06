d7fcf7ead53136f1ecabec6c728b4a69ddf97bf2
Andrea Parodi <a.parodi@inet2.it>
2015-08-06 17:23:54 +0200

    first release

--- index.js
+++ index.js
@@ -1,13 +1,148 @@
-import { fill } from 'git-credential-helper';
+import { spawnSync, execFile } from 'child_process';
 
-export default async function gsw() {
-  return new Promise( (resolve, reject) => {
-    fill('github.com', (err, data)=> {
+const credentialRE = /username=([^\n]+)\npassword=([^\n]+)\n/;
+
+function parse(result) {
+  const match = result.match(credentialRE);
+  if (!match) {
+    return null;
+  }
+  const [_, username, password] = match; // eslint-disable-line no-unused-vars
+
+  return { username, password };
+}
+
+function runSync(command, opts) {
+  const result = spawnSync('git', ['credential', command], opts);
+
+  if (result.error) {
+    throw result.error;
+  }
+  if (result.stderr) {
+    throw new Error(result.stderr);
+  }
+
+  return result.stdout;
+}
+
+
+function runAsync(command, opts, cb) {
+  const proc = execFile('git', ['credential', command], opts, (err, stdout, stderr) => {
+    if (err) {
+      return cb(err);
+    }
+    if (stderr) {
+      return cb(new Error(stderr));
+    }
+
+    cb(null, stdout);
+  });
+
+  if (opts.input) {
+    proc.stdin.write(opts.input);
+  }
+}
+
+function fillOpts(url) {
+  return {
+    encoding: 'utf8',
+    input: url ? `url=${url}\n\n` : '\n',
+    env: Object.assign({GIT_TERMINAL_PROMPT: '0'}, process.env)
+  };
+}
+
+export function fillSync(url) {
+  try {
+    const result = runSync('fill', fillOpts(url));
+    return parse(result);
+  } catch (err) {
+    if (err.message.includes('terminal prompts disabled')) {
+      return null;
+    }
+    throw err;
+  }
+}
+
+export function fill(url, cb) {
+  return new Promise( (resolve, rej) => {
+    runAsync('fill', fillOpts(url), (err, result) => {
       if (err) {
-        return reject(err);
-      }
+        if (err.message.includes('terminal prompts disabled')) {
+          if (cb) {
+            cb(null, null);
+          }
+          return resolve(null);
+        }
 
-      return resolve(data);
-    }, { silent: true });
+        if (cb) {
+          cb(err);
+        }
+        return rej(err);
+      }
+      const ret = parse(result);
+      resolve(ret);
+      if (cb) {
+        cb(null, ret);
+      }
+    });
   });
 }
+
+function rejectOpts(url) {
+  return {
+    encoding: 'utf8',
+    input: url ? `url=${url}\n\n` : '\n'
+  };
+}
+
+export function rejectSync(url) {
+  runSync('reject', rejectOpts(url));
+}
+
+export function reject(url, cb) {
+  return new Promise( (resolve, rej) => {
+    runAsync('reject', rejectOpts(url), err => {
+      if (err) {
+        if (cb) {
+          cb(err);
+        }
+        return rej(err);
+      }
+      resolve(null);
+      if (cb) {
+        cb(null, null);
+      }
+    });
+  });
+}
+
+function approveOpts({ username, password, url }) {
+  return {
+    encoding: 'utf8',
+    input: (url ? `url=${url}\n` : '') +
+      `username=${username}\npassword=${password}\n\n`
+  };
+}
+
+export function approveSync({ username, password, url }) {
+  runSync('approve', approveOpts({ username, password, url }));
+}
+
+
+export function approve({ username, password, url }, cb) {
+  return new Promise( (resolve, rej) => {
+    runAsync('approve', approveOpts({ username, password, url }), err => {
+      if (err) {
+        if (cb) {
+          cb(err);
+        }
+        return rej(err);
+      }
+      resolve(null);
+      if (cb) {
+        cb(null, null);
+      }
+    });
+  });
+}
+
--- test/index_test.js
+++ test/index_test.js
@@ -1,13 +1,94 @@
-import gsw from '../index';
+import { fill, approve, reject } from '../index';
+import { fillSync, approveSync, rejectSync } from '../index';
+import thenify from 'thenify';
 
-describe('git-switch', () => {
-  it('is defined', () => {
-    gsw.should.be.a('function');
+const testWith = ({ _fill, _approve, _reject }) => () => {
+  const save = () => _approve({
+    url: 'https://myg.itho.st',
+    username: 'myusername',
+    password: 'piripicchio'
   });
 
-  it('support async', async () => {
-    const result = await Promise.resolve(gsw());
-    console.dir(result)
-    result.should.be.equal(42);
+  describe('approve', () => {
+    let result;
+
+    after( async () => {
+      await _reject('https://myg.itho.st');
+    });
+
+    before( async () => {
+      await _reject('https://myg.itho.st');
+      await save();
+      result = await _fill('https://myg.itho.st');
+    });
+
+    it('store username', () => {
+      result.username.should.be.equal('myusername');
+    });
+
+    it('store password', () => {
+      result.password.should.be.equal('piripicchio');
+    });
   });
+
+
+  describe('reject', () => {
+    let result;
+
+    before(async () => {
+      await save();
+      await _reject('https://myg.itho.st');
+      result = await _fill('https://myg.itho.st');
+    });
+
+    it('credentials are removed', () => {
+      should.equal(result, null);
+    });
+  });
+
+  describe('fill', () => {
+    let result;
+
+    before(async () => {
+      await save();
+      result = await _fill('https://myg.itho.st');
+    });
+
+    it('is defined', () => {
+      _fill.should.be.a('function');
+    });
+
+    it('return an object', () => {
+      result.should.be.a('object');
+    });
+
+    it('with username', () => {
+      result.username.should.be.equal('myusername');
+    });
+
+    it('with password', () => {
+      result.password.should.be.equal('piripicchio');
+    });
+  });
+};
+
+describe('git-credential-sync', () => {
+  describe('sync', testWith({
+    _fill: fillSync,
+    _approve: approveSync,
+    _reject: rejectSync
+  }));
+
+  describe('async using promises', testWith({
+    _fill: fill,
+    _approve: approve,
+    _reject: reject
+  }));
+
+  describe('async using callbacks', testWith({
+    _fill: thenify(fill),
+    _approve: thenify(approve),
+    _reject: thenify(reject)
+  }));
 });
+
